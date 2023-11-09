ff9f39b27dc5918e8cd7b25088ad147cb22ec61f
George Chartouny <chartounygeorge@gmail.com>
2023-11-09 16:29:32 +0200

    remove useEffect

--- ni-frontend/src/pages/Dashboard.tsx
+++ ni-frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { getAggData } from "../api/aggregateDataApi";
 
 export const Dashboard = () => {
