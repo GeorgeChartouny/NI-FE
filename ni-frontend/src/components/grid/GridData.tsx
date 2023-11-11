2cc58bf7ec0029c51b271a689a723c19c2d3a180
George Chartouny <chartounygeorge@gmail.com>
2023-11-11 18:18:56 +0200

    table body schema

--- ni-frontend/src/components/grid/GridData.tsx
+++ ni-frontend/src/components/grid/GridData.tsx
@@ -1,43 +1,61 @@
-import React from 'react'
+import React from "react";
 import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper"
-import Table from "@mui/material/Table"
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
-import TableCell from "@mui/material/TableCell"
+import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-
+import { TableBody } from "@mui/material";
 
 export const GridData = () => {
-    const headTableStyle = {
-        fontWeight: "bold",
-        fontSize: "0.8rem",
-        textAlign: "left",
-      };
-  return (
-  <>
-  <TableContainer component={Paper}
-  sx={{
-    borderRadius:"15px",
-    height:"100%",
-    padding:"5px",
-    overflowY:"scroll"
-  }}
-  >
-    <Table sx={{minWidth:"650px"}} arial-label="simple table">
-        <TableHead>
-            <TableRow>
+  const HeadTableStyle = {
+    fontWeight: "bold",
+    fontSize: "0.8rem",
+    textAlign: "left",
+  };
 
-            <TableCell sx={headTableStyle}>DATETIME KEY</TableCell>
-            <TableCell sx={headTableStyle}>Time</TableCell>
-            <TableCell sx={headTableStyle}>NE Type</TableCell>
-            <TableCell sx={headTableStyle}>NE Alias</TableCell>
-            <TableCell sx={headTableStyle}>RSL INPUT POWER</TableCell>
-            <TableCell sx={headTableStyle}>MAX RX LEVEL</TableCell>
-            <TableCell sx={headTableStyle}>RSL DEVIATION</TableCell>
+  const BodyTableStyle = {
+    fontSize: "0.8rem",
+    textAlign: "left",
+  };
+
+  return (
+    <>
+      <TableContainer
+        component={Paper}
+        sx={{
+          borderRadius: "15px",
+          height: "100%",
+          padding: "5px",
+          overflowY: "scroll",
+        }}
+      >
+        <Table sx={{ minWidth: "650px" }} arial-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={HeadTableStyle}>DATETIME KEY</TableCell>
+              <TableCell sx={HeadTableStyle}>Time</TableCell>
+              <TableCell sx={HeadTableStyle}>NE Type</TableCell>
+              <TableCell sx={HeadTableStyle}>NE Alias</TableCell>
+              <TableCell sx={HeadTableStyle}>RSL INPUT POWER</TableCell>
+              <TableCell sx={HeadTableStyle}>MAX RX LEVEL</TableCell>
+              <TableCell sx={HeadTableStyle}>RSL DEVIATION</TableCell>
             </TableRow>
-        </TableHead>
-    </Table>
-  </TableContainer>
-  </>
-  )
-}
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+              <TableCell sx={BodyTableStyle}>Value Here</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
