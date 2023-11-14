9b9b31bbd0104b29d2360d89615c44ad11bfabd2
George Chartouny <chartounygeorge@gmail.com>
2023-11-14 12:54:28 +0200

    display loading screen 1 second on refresh

--- ni-frontend/src/components/loading animation/LoadingAnimation.tsx
+++ ni-frontend/src/components/loading animation/LoadingAnimation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./loadingAnimation.css"
 
 export const LoadingAnimation = () => {
   return (
--- ni-frontend/src/pages/Dashboard.tsx
+++ ni-frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,23 @@
-
+import {useEffect,useState} from "react";
 import { LineChartComponent } from "../components/chart/LineChartComponent";
 import { GridData } from "../components/grid/GridData";
 import { Menu } from "../components/menu/menu";
 import styles from "./Dashboard.styles";
+import { Loading } from "../components/loading/Loading";
 
 export const Dashboard = () => {
+const [isLoading, setIsLoading] = useState<boolean>(true);
 
+
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setIsLoading(false); // Set isLoading to false after 2 seconds
+  }, 1000);
+
+  return () => clearTimeout(timer); // Cleanup timer on component unmount
+}, []);
+
+if(isLoading) return <Loading/>
  return (
   <styles.Container>
     <Menu/>
