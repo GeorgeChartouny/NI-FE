dc6c30cef6b497dfcb7427d94fff17e400a637fa
George Chartouny <chartounygeorge@gmail.com>
2023-11-14 11:18:15 +0200

    linechart with data

--- ni-frontend/src/components/chart/LineChartComponent.tsx
+++ ni-frontend/src/components/chart/LineChartComponent.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import {ChartDataType} from "../../types/types";
-import { LineChart } from "@mui/x-charts/LineChart";
+// import { LineChart } from "@mui/x-charts/LineChart";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { Container, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
+import {format} from "date-fns";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 export const LineChartComponent = () => {
   const { isFetching, error, data, aggTime } = useSelector(
@@ -14,25 +17,30 @@ export const LineChartComponent = () => {
   let rslInputPowerData: ChartDataType[] = [];
   let maxRxLevel: ChartDataType[] = [];
 
+  const chartData  = data.map((item) => ({
+    time: new Date(item.timE_Stamp).toLocaleDateString(), // Formatting timestamp as per requirement
+    kpi: item.maX_RX_LEVEL,
+  }));
+
   const FilterAggData = () => {
     data.map((d) => {
       rslDeviationData.push({
-        time: d.timE_Stamp,
+        time: new Date(d.timE_Stamp).toLocaleDateString(),
         value: d.rsL_DEVIATION,
       });
       rslInputPowerData.push({
-        time: d.timE_Stamp,
+        time: new Date(d.timE_Stamp).toLocaleDateString(),
         value: d.rsL_INPUT_POWER,
       });
       maxRxLevel.push({
-        time: d.timE_Stamp,
+        time: new Date(d.timE_Stamp).toLocaleDateString(),
         value: d.maX_RX_LEVEL,
       });
     });
   };
 
   useEffect(() => {
-
+    console.log('chartData', chartData)
     FilterAggData();
   }, [data]);
 
@@ -46,25 +54,45 @@ export const LineChartComponent = () => {
             height: "50%",
             width: "70%",
             padding: "5px",
-            overflowY: "scroll",
+            overflowX: "scroll",
+
           }}
         >
-          <Typography variant="h5" sx={{ mb: 2 }}>
+             <Typography variant="h5" sx={{ mb: 2 }}>
             KPIs
-          </Typography>
+          </Typography> 
+        <ResponsiveContainer width="100%" height="85%">
+        
           <LineChart
-            colors={["#790b0c"]}
+            // colors={["#790b0c"]}
             // leftAxis={[labelFontSize:"20",]}
-            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-            series={[
-              {
-                data: data,
-              },
-            ]}
-            width={500}
-            height={300}
-          />
-        </Container>
+          //   xAxis={[
+          //     { 
+          //       dataKey: 'time',
+                
+          //      }
+          //     ]}
+          //   series={[
+          //     {
+          //       dataKey: 'value',
+          //     },
+          //   ]}
+          //   width={500}
+          //   height={300}
+          // dataset={chartData}
+          data = {chartData}
+            
+          >
+                    {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                    <XAxis dataKey="time" />
+                    <YAxis />
+                   <Tooltip />
+                   <Legend />
+                   <Line type="monotone" dataKey="kpi" stroke="#790b0c" strokeWidth={2} />
+
+            </LineChart>
+            </ResponsiveContainer>
+         </Container>
       ) : (
         <></>
       )}
