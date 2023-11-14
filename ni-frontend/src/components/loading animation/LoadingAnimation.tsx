ada776456a0f0185ecb041f814b0bfde0ea0dbd1
George Chartouny <chartounygeorge@gmail.com>
2023-11-14 12:42:58 +0200

    adding files

---
+++ ni-frontend/src/components/loading animation/LoadingAnimation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const LoadingAnimation = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
--- ni-frontend/src/components/loading/Loading.tsx
+++ ni-frontend/src/components/loading/Loading.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Loading.styles";
+import { LoadingAnimation } from "../loading animation/LoadingAnimation";
 
 export const Loading = () => {
   return (
     <styles.Container>
+      <LoadingAnimation/>
       <styles.CompanyLogo />
-      Loading
     </styles.Container>
   );
 };
